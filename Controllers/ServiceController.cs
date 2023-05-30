fb8895b84f43087338270aed074d731a5031063e
ChotaBeck <mulenga99@gmail.com>
2023-05-30 17:38:20 +0200

    adding serviceController

---
+++ Controllers/ServiceController.cs
@@ -0,0 +1,96 @@
+using Microsoft.AspNetCore.Mvc;
+using SprintathonAPI.Models;
+using SprintathonAPI.Data;
+
+namespace SprintathonAPI.Controllers;
+
+[Route("api/[contoller]")]
+[ApiController]
+public class ServiceController : ControllerBase
+{
+    private readonly ApplicationDbContext _db;
+    private readonly IConfiguration _configuration;
+
+    public ServiceController(ApplicationDbContext context, IConfiguration configuration)
+    {
+        _db = context;
+        _configuration = configuration;
+    }
+
+    //GET 
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Service>>> GetServices()
+    {
+        return await _db.Services.ToListAsync();   
+    }
+
+    //GET
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Service>> GetServiceObj(int id)
+    {   
+        var serviceObj = await _db.Services.FindAsync(id);
+        
+        if(serviceObj ==null)
+            return NotFound();
+        
+        return serviceObj;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Service>> PutServiceObj(int id, Service serviceObj)
+    {   
+        if(id != _db.Services.id)
+        {
+            return BadRequest();
+        }
+
+        _db.Entry(serviceObj).State = EntityState.Modified;
+
+        
+        try
+        {
+            await _db.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!ServiceExists(serviceObj.Id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+        return NoContent();
+        
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Service>> PostServiceObj(Service serviceObj)
+    {
+        _db.Service.Add(serviceObj);
+        await _db.SaveChangesAsync();
+
+        return CreatedAtAction("GetExampleEntity", new { id = serviceObj.Id }, serviceObj);
+    }
+
+    // DELETE: api/Example/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Service>> DeleteServiceObj(int id)
+        {
+            var serviceObj = await _db.Examples.FindAsync(id);
+            if (serviceObj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Examples.Remove(serviceObj);
+            await _db.SaveChangesAsync();
+
+            return serviceObj;
+        }
+
+        
+
+}

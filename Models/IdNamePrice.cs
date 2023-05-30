214c71af10919010009eed0a057835692e1a0bc3
Philimon Kaminga <philimonkaminga@gmail.com>
2023-05-30 13:11:49 +0200

    Base classes added

---
+++ Models/IdNamePrice.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.SqlTypes;
+
+namespace SprintathonAPI.Models
+{
+    public class IdNamePrice
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}

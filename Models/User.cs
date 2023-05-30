2f354dc9a1749eaac7e9dd8781eade17a2cc906f
mulengfi <fidelitychileshemulenga@gmail.com>
2023-05-30 14:47:26 +0100

    Adding the user entity class

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+
+public class User: IdNameBase
+{
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public string email { get; set; }
+    public string password { get; set; }
+    public string location { get; set; }
+    public string phoneNumber { get; set; }
+
+    public Name =$"{firstName} {lastName}";
+    
+}

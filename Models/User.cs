9f8b2d775f318d9612c1258dd8cf9f763b49435b
IttaiTumelo <ittaitumelo@outlook.com>
2023-05-31 05:11:46 +0200

    User Update

--- Models/User.cs
+++ Models/User.cs
@@ -8,7 +8,17 @@ namespace SprintathonAPI.Models {
         public string Password { get; set; }
         public string Location { get; set; }
         public string PhoneNumber { get; set; }
+    }
 
-       
+    public class UserLogInDTo
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class CreateUserDto : User
+    {
+        public readonly int Id = 0;
+        public string Name { get=> $"{FirstName} {LastName}";}
     }
 }
